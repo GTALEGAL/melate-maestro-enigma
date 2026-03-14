4d08adb257b101c6cef16ad2a925f8625db86f18
Asesoria Legal <cugtc.legal.mx@gmail.com>
2026-03-14 09:27:23 -0600

    Update comunicacion.py

--- api/comunicacion.py
+++ api/comunicacion.py
@@ -1,19 +1,9 @@
 from http.server import BaseHTTPRequestHandler
-import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/plain') # Usamos texto simple
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        
-        # Respuesta ultra simple
-        self.wfile.write(json.dumps({"letra": "Z"}).encode())
-
-    def do_OPTIONS(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-        self.end_headers()
+        self.wfile.write("Z".encode()) # Solo enviamos la letra pura
