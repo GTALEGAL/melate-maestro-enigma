770d20935fb75f60cec0b9bd766a9ca244ca0869
Asesoria Legal <cugtc.legal.mx@gmail.com>
2026-03-13 22:01:39 -0600

    Update equipo1.py

--- api/equipo1.py
+++ api/equipo1.py
@@ -11,14 +11,13 @@ class handler(BaseHTTPRequestHandler):
             
             # CAPA 1: Frecuencia Simple
             todos = df[['R1','R2','R3','R4','R5','R6','R7']].values.flatten()
-            comunes = [n for n, c in Counter(todos).most_common(15)]
-            pool = sorted(comunes[:7])
+            pool = [int(n) for n, c in Counter(todos).most_common(7)]
 
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', '*') # PARCHE
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
-            self.wfile.write(json.dumps({"equipo": "Eq1", "pool": pool, "objetivo": "C1-Frecuencia"}).encode())
+            self.wfile.write(json.dumps({"equipo": "Eq1", "pool": sorted(pool), "objetivo": "C1-Frecuencia"}).encode())
         except Exception as e:
             self.send_response(200); self.send_header('Access-Control-Allow-Origin', '*'); self.end_headers()
             self.wfile.write(json.dumps({"error": str(e)}).encode())
